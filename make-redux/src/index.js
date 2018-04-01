902376abb22c8ee4049b2bb1dc8f3696fdba8abd
wkcoder <997972547@qq.com>
2018-04-01 20:57:36 +0800

    make redux first commit

--- make-redux/src/index.js
+++ make-redux/src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+const appState = {
+    title:{
+        text:'React Redux Title',
+        color:'red'
+    },
+    content:{
+        text:'React Redux Content',
+        color:'blue'
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+function renderApp(appState) {
+    renderTitle(appState.title)
+    renderContent(appState.content)
+}
+
+function renderTitle(title) {
+    const titleDOM = document.getElementById('title')
+    titleDOM.innerHTML = title.text
+    titleDOM.style.color = title.color
+}
+
+function renderContent(content) {
+    const contentDOM = document.getElementById('content')
+    contentDOM.innerHTML = content.text
+    contentDOM.style.color = content.color
+}
+
+renderApp(appState)
