25f7179257db9266be8998aca03849e6f3021085
wkcoder <997972547@qq.com>
2018-04-03 11:05:34 +0800

    sz redux

--- make-redux/src/index.js
+++ make-redux/src/index.js
@@ -1,4 +1,4 @@
-const appState = {
+let appState = {
     title:{
         text:'React Redux Title',
         color:'red'
@@ -27,4 +27,34 @@ function renderContent(content) {
     contentDOM.style.color = content.color
 }
 
-renderApp(appState)
+function stateChanger(state,action) { // dispatch action
+    switch (action.type){
+        case 'UPDATE_TITLE_TEXT':
+            state.title.text = action.text
+            break
+        case 'UPDATE_TITLE_COLOR':
+            state.title.color = action.color
+            break
+        default:
+            break
+    }
+}
+
+function createStore(state,stateChanger) {
+    const listeners = []
+    const subscribe  = (listener) => listeners.push(listener)
+    const getState = () => state
+    const dispatch = (action) =>{stateChanger(state,action)
+        listeners.forEach((listener) => listener())
+    }
+
+    return { getState , dispatch ,subscribe}
+}
+
+
+const store = createStore(appState,stateChanger)
+store.subscribe(() =>renderApp(store.getState()))
+console.log(store)
+renderApp(store.getState())
+store.dispatch({type:'UPDATE_TITLE_TEXT',text:'React Action'})
+store.dispatch({type:'UPDATE_TITLE_COLOR',color:'green'})
