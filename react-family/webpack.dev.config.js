494273d31b4fd17dd6700f28333bf2475bef53bf
wangzilaogong <997972547@qq.com>
2018-04-25 14:43:49 +0800

    wk

--- react-family/webpack.dev.config.js
+++ react-family/webpack.dev.config.js
@@ -9,6 +9,7 @@ const devConfig = {
         app: [
             'babel-polyfill',
             'react-hot-loader/patch',
+            
             path.join(__dirname, 'src/index.js')
         ]
     },
