3a534db7eecbe6da5451f9b421970dc12a7f3d95
吴凯 <wukai@wukaideMacBook-Pro.local>
2018-04-25 14:27:38 +0800

    done

--- react-family/webpack.dev.config.js
+++ react-family/webpack.dev.config.js
@@ -1,63 +1,40 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge');
+const path = require('path');
 
-module.exports = {
-    entry :{
-    app:[
-        'babel-polyfill',
-        'react-hot-loader/patch',
-        path.join(__dirname,'src/index.js')
-    ],
-    vendor:['react','react-router-dom','redux','react-dom','react-redux']
-},  
-    
+const commonConfig = require('./webpack.common.config.js');
+
+const devConfig = {
+    devtool: 'inline-source-map',
+    entry: {
+        app: [
+            'babel-polyfill',
+            'react-hot-loader/patch',
+            path.join(__dirname, 'src/index.js')
+        ]
+    },
     output: {
-        path:path.join(__dirname,'./dist'),
-        filename:'[name].[hash].js',
-        chunkFilename:'[name].[chunkhash].js'
+        /*这里本来应该是[chunkhash]的，但是由于[chunkhash]和react-hot-loader不兼容。只能妥协*/
+        filename: '[name].[hash].js'
     },
     module: {
         rules: [{
-            test: /\.js$/,
-            use: ['babel-loader?cacheDirectory=true'],
-            include: path.join(__dirname, 'src')
-        },
-        {
-            test:/\.css$/,
-            use:['style-loader','css-loader']
-        },
-        {
-            test:/\.(png|jpg|png|gif)$/,
-            use:[{
-                loader:'url-loader',
-                options:{
-                    limit:24576
-                }
-            }]
-        },
-        ]
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+        }]
     },
-    plugins:[ new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: path.join(__dirname,'src/index.html')
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-        name:'vendor'
-    })
-],
-    resolve: {
-        alias: {
-            pages: path.join(__dirname, 'src/pages'),
-            component: path.join(__dirname, 'src/component'),
-            router: path.join(__dirname, 'src/router'), 
-            actions: path.join(__dirname, 'src/redux/actions'),
-            reducers: path.join(__dirname, 'src/redux/reducers'),
-        }
-    },
-     devServer: {
-        contentBase: path.join(__dirname, './dist')
-    },
-    
-    devtool:'inline-source-map'
+    devServer: {
+        contentBase: path.join(__dirname, './dist'),
+        historyApiFallback: true,
+        host: '0.0.0.0',
+    }
 };
+
+module.exports = merge({
+    customizeArray(a, b, key) {
+        /*entry.app不合并，全替换*/
+        if (key === 'entry.app') {
+            return b;
+        }
+        return undefined;
+    }
+})(commonConfig, devConfig);
